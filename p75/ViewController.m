d8ad0257c477f833b6b5c73cf173bc574f37c969
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-03 13:54:59 +0900

    프로젝트
    p75

--- p75/ViewController.m
+++ p75/ViewController.m
@@ -8,16 +8,58 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    UISegmentedControl *segment;
+}
 
 @end
 
 @implementation ViewController
+- (IBAction)segmentedC1:(id)sender {
+    UISegmentedControl *control = (UISegmentedControl *)sender;
+    NSString * selectedStr = [control titleForSegmentAtIndex:control.selectedSegmentIndex];
+    NSLog(@"Index: %d - Title: %@", control.selectedSegmentIndex,selectedStr);
+}
+- (void) segball:(UISegmentedControl *)sender{
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            NSLog(@"Baseball");
+        break;
+            
+        case 1:
+            NSLog(@"basketball");
+            break;
+            
+        case 2:
+            NSLog(@"Football");
+            break;
+            
+        case 3:
+            NSLog(@"volleyball");
+            break;
+            
+        default:
+            break;
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    UIImage *base = [UIImage imageNamed:@"baseball.png"];
+    UIImage *foot = [UIImage imageNamed:@"football.png"];
+    UIImage *basket= [UIImage imageNamed:@"basket.png"];
+    UIImage *volley= [UIImage imageNamed:@"volley.png"];
+    NSArray *items = [NSArray arrayWithObjects:base, foot, basket, volley,nil];
+    
+    segment = [[UISegmentedControl alloc]initWithItems:items];
+    segment.frame = CGRectMake(10, 100, 300, 40);
+    [self.view addSubview:segment];
+    
+    segment.selectedSegmentIndex=2;
+    
+    [segment addTarget:self action:@selector(segball:) forControlEvents:UIControlEventValueChanged];
 }
 
 - (void)didReceiveMemoryWarning
